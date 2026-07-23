356bb7e4f20b5547265a52e4ec6ba12b1e09ce32
Maanik Arya <maanikarya200@gmail.com>
2026-07-23 17:14:03 +0530

    Implement product details view and service method for fetching product by ID

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/models/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -9,5 +12,20 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent {
+   // Get the Id from the parmeter
+   id: number = 0; 
+   product : Product | undefined; 
+   constructor(private route: ActivatedRoute, private productService: ProductService){
+   }
 
+   ngOnInit():void
+   {
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+    if(this.id){
+      this.productService.getProductById(this.id).subscribe((response) => {
+          this.product = response; 
+          console.log(this.product);
+      });
+    }
+   }
 }
